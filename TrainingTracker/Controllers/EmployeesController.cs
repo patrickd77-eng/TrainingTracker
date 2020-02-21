d2e46922b7223b6b00821223635e90e71aeb92e6
Patrick Davis <patrick_davis97@hotmail.co.uk>
2020-02-21 10:18:47 +0000

    Switching to scaffolded CRUD operations. No longer using custom controller/view/javascript.

--- TrainingTracker/Controllers/EmployeesController.cs
+++ TrainingTracker/Controllers/EmployeesController.cs
@@ -2,94 +2,152 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TrainingTracker.Data;
 using TrainingTracker.Models;
 
 namespace TrainingTracker.Controllers
 {
-    [Authorize]
     public class EmployeesController : Controller
     {
-        public EmployeesController(ApplicationDbContext db)
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context)
         {
-            _db = db;
+            _context = context;
         }
 
-        private readonly ApplicationDbContext _db;
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
 
-        [BindProperty]
-        public Employee Employee { get; set; }
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        //View for employee list.
-        public IActionResult Index()
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
         {
             return View();
         }
 
-        
-        //ID is nullable here (not required). Return an employee's info or a not found error based on ID presence.
-        public IActionResult Update(int? id)
-        {
-            Employee = new Employee();
-            if (id == null)
-            {       
-                return View(Employee);
-            }
-          
-            Employee = _db.Employees.FirstOrDefault(u => u.Id == id);
-            if (Employee == null)
-            {
-                return NotFound();
-            }
-            return View(Employee);
-        }
-
-        #region Database/API Calls
-        //POST a new employee (If no ID given, this is a create request. If ID present, it's update.)
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Update()
+        public async Task<IActionResult> Create([Bind("Id,fullName,overallProgress")] Employee employee)
         {
             if (ModelState.IsValid)
             {
-                if (Employee.Id == 0)
-                {           
-                    _db.Employees.Add(Employee);
-                }
-                else
-                {            
-                    _db.Employees.Update(Employee);
-                }
-                _db.SaveChanges();
-                //Redirect back to list view.
-                return RedirectToAction("Index");
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
-            return View(Employee);
+            return View(employee);
         }
 
-        //GET a list of all Employees.
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            return Json(new { data = await _db.Employees.ToListAsync() });
-        }
-
-        //Send a DELETE request for this particular Employee. The ID of said employee is passed from Ajax to this method.
-        [HttpDelete]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var employeeFromDb = await _db.Employees.FirstOrDefaultAsync(u => u.Id == id);
-            if (employeeFromDb == null)
+            if (id == null)
             {
-                return Json(new { success = false, message = "Error while deleting the employee." });
+                return NotFound();
             }
-            _db.Employees.Remove(employeeFromDb);
-            await _db.SaveChangesAsync();
-            return Json(new { success = true, message = "Delete successful." });
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
         }
-        #endregion
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,fullName,overallProgress")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
     }
 }
