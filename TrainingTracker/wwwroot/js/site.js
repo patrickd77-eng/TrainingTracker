5132175f20e2520b5731fe25eaafa3a8e29aad77
Patrick Davis <patrick_davis97@hotmail.co.uk>
2020-02-28 18:05:09 +0000

    Added some completed stats to the progress page. Also a scroll down and scroll up button.

--- TrainingTracker/wwwroot/js/site.js
+++ TrainingTracker/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+function scrollBottom() {
 
-// Write your JavaScript code.
+    $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+
+};
+
+function scrollUp() {
+
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+
+};
