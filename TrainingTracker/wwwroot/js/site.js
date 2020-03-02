f69a0dab6109b11cccea76f72f790564839857db
Patrick Davis <patrick_davis97@hotmail.co.uk>
2020-03-02 23:37:08 +0000

    Added ability to lock progress actions section incase of user misclick.

--- TrainingTracker/wwwroot/js/site.js
+++ TrainingTracker/wwwroot/js/site.js
@@ -16,6 +16,16 @@ $(".open").on("click", function() {
     $(".popup-content").addClass("active");
     $(popupWarning).html("<span class=\"alert-success\">Unlocked.</span> <br> Proceed with caution.")
     $(openPopup).hide();
+    $(closeButton).css("visibility", "unset");
     $(popupLockText).hide();
     $(popupContainer).css("height", "100%");
+});
+
+$(closeButton).on("click", function() {
+    $(".popup-content, .popup-overlay").removeClass("active");
+    $(popupWarning).html("<span class=\"alert-danger\">Locked.</span> <br> Proceed with caution.")
+    $(openPopup).show();
+    $(closeButton).css("visibility", "hidden");
+    $(popupLockText).show();
+    $(popupContainer).css("height", "0");
 });
